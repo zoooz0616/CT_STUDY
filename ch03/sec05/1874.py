9c88000a2343ef20f9b3c211fbad0e8c8bcdbc6f
zoooz0616 <53183320+zoooz0616@users.noreply.github.com>
2023-04-10 23:22:31 +0900

    codingtest study

---
+++ ch03/sec05/1874.py
@@ -0,0 +1,36 @@
+N = int(input())
+A = [0]*N
+
+for i in range(N):
+    A[i]=int(input())
+
+stack = []
+num =1
+result = True
+answer = " "
+
+for i in range(N):
+    su = A[i]
+    if su >= num:
+        while su >= num:
+            stack.append(num)
+            num += 1
+            answer += "+\n"
+
+        stack.pop()
+        answer += "-\n"
+
+    else:
+        n=stack.pop()
+
+        if n > su:
+            print("NO")
+            result=False
+            break
+
+        else:
+            answer += "-\n"
+
+if result:
+    print(answer)
+
