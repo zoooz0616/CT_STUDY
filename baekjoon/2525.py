a948b75e71e310e27d2842ff1204d0e31cd4fc02
KOSA <zoooz0616@gmail.com>
2023-04-13 17:52:52 +0900

    baekjoon code middle commit

--- baekjoon/2525.py
+++ baekjoon/2525.py
@@ -1,10 +1,10 @@
 a,b =map(int, input().split())
 c=int(input())
 
-if b+c>60:
+if b+c>=60:
     a += (b+c)//60
     b = (b+c)%60
-    if a >24:
+    if a >=24:
         a = a%24
         print(a, b)
     else: print(a,b)
